de52be41c27452495924b2f7e10b1da94a1e6be2
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-09 18:05:07 +0100

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - this is rhe main function of this program
+ * Description: this progrm prints hexadecimak numbers in lowercase
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int digit, letter;
+
+	for (digit = 0; digit <= 9; digit++)
+	{
+		putchar(digit);
+	}
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
