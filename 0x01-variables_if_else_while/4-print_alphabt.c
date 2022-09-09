91208637d5379dc7a0826425c5e70c9f9ce97d26
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-09 17:21:21 +0100

    remove q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - this function will execute the functions
+ * Description: prints all alphabets except q and e
+ *
+ * Return: returns 0. success value
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			if (letter != 'q' || letter != 'e')
+			{
+				putchar(letter);
+			}
+		}
+	putchar('\n');
+}
