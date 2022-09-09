2e107a3ccd22bcb581ba5ea4e4dee8ade6d52719
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-09 18:25:37 +0100

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,16 +9,17 @@
 
 int main(void)
 {
-	int digit;
+	char digit;
 
 	for (digit = '0'; digit < '10'; digit++)
 	{
 		putchar(digit);
 		if (digit != 9)
 		{
-			putchar(', ');
+			putchar(',');
+			putchar(' ');
 		}
 	}
-	putchar('\n);
+	putchar('\n');
 	return (0);
 }
