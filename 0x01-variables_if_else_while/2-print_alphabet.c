47dd035151b71f6085f479fbad23b9c2ea1673c2
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-09 16:36:59 +0100

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - this is where functions are called
+ * Description: this program prints all the alphabet in lowercase
+ *
+ * Return: Always return 0. Succes result.
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch < 'z'; ch++)
+	{
+		putchar(ch);
+		putchar('\n');
+	}
+	return (0);
+}
