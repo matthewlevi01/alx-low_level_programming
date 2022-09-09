54607702922c6b2513e0d57a48bd684b68d32712
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-09 17:42:44 +0100

    print alphabets backwards

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - exectures all functions
+ * Description: main prints letters backwards
+ *
+ * Return: returns 0. succes value
+ *
+ */
+
+int main(void)
+{
+	char letter = 'z';
+
+	for (letter = 'z'; letter <= 'a'; letter--)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
