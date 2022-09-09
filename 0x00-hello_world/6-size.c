ade85bb7fe7228cc8dd6c01ff6033dc526b489bb
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-09 05:26:45 +0100

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,11 +2,11 @@
 
 /**
  * main - Entry point
- * This program prints the various sizes
+ * Description: This program prints the various sizes
  * of various types compiled and run
  *
  * And it is to return a 0 as output
- * Return : Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -15,6 +15,6 @@ int main(void)
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %f bytes(s)\n", sizeof(float));
+	printf("Size of a float: %d bytes(s)\n", sizeof(float));
 	return (0);
 }
