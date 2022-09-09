f4102acf5b5346add206fc2d185c34eac09fc922
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-09 18:20:13 +0100

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Description: This prints numbers and commas
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	char digit;
+
+	for (digit = '0'; digit < '10'; digit++)
+	{
+		putchar(digit);
+		if (digit != 9)
+		{
+			putchar(', ');
+		}
+	}
+	putchar('\n);
+	return (0);
+}
