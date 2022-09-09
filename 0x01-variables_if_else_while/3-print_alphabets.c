bd67ec44a7900bbfeccc2f45d74e73b15036c7fb
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-09 16:47:13 +0100

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this is the function that will exc ute other functions
+ * Description: This program prints lowercase letters, then upper case letters
+ *
+ * Return: Returns 0 always. Success value
+ */
+
+int main(void)
+{
+	char letter = 'a'
+
+		for (letter = 'a'; letter <= 'Z'; letter++)
+			putchar(letter);
+	putchar('\n');
+	return (0);
+}
