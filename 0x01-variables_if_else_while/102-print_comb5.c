fde3e890a77d4f60069b74a914b8e4de16ffb935
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-10 15:19:57 +0100

    print range

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,11 +13,11 @@ int main(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		for (j = i; j <= '9'; j++)
+		for (j = 0; j <= '9'; j++)
 		{
-			for (k = j; k <= '9'; k++)
+			for (k = 0; k <= '9'; k++)
 			{
-				for (l = k; l <= '9'; l++)
+				for (l = 1; l <= '9'; l++)
 				{
 					putchar(i);
 					putchar(j);
