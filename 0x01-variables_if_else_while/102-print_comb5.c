bf9f7c4d6f45b7e099d09dc427d4e2d1eda3e84a
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-10 16:40:55 +0100

    print range

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,9 +13,22 @@ int main(void)
 
 	for (i = 0; i < 100; i++)
 	{
-		putchar(i / 10);
-		putchar(i % 10);
-		putchar(' ');
+		for(j = 0; j < 100; j++)
+		{
+			if (i < j)
+			{
+				putchar((i /10) +48);
+				putchar((i % 10) +48);
+				putchar(' ');
+				putchar((j / 10) +48);
+				putchar((j % 10) +48);
+				if ( i != 98 || j != 99)
+				{
+					putchar(',');
+					puchar(' ');
+				}
+			}
+		}
 	}
 	putchar('\n');
 	return (0);
