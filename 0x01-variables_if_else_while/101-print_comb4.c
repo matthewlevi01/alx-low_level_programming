c5ed0fc24abb7c130d2bdee3da121ae26e813dde
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-10 08:01:29 +0100

    tripple

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Description: this function prints all possible combination of three
+ * numbers
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			for (k = j + 1; k <= 9 k++)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar('0' + k);
+				if (k != 9 || j != 8 || i != 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
