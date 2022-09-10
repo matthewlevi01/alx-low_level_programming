afd5333389973341fc720ff6899de397ebb6af62
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-10 14:18:34 +0100

    print range

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,35 +1,27 @@
 #include <stdio.h>
 
 /**
- * main - this is the main function
- * Description: this function prints all possible combination of three
- * numbers
+ * main - this is the main executor function
+ * Description: prints a combination of digits from 0 - 99
  *
- * Return: returns 0
+ * Return: returns 0. Success value
  */
 
 int main(void)
 {
-	int i, j, k;
+	int i, j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		for (j = i + 1; j <= 9; j++)
+		for (j = '0'; j <= '9'; j++)
 		{
-			for (k = j + 1; k <= 9; k++)
+			for (k = '0'; k <= '9'; k++)
 			{
 				putchar('0' + i);
 				putchar('0' + j);
 				putchar('0' + k);
-				if (k != 9 || j != 8 || i != 7)
-				{
-					putchar(',');
-					putchar(' ');
-				}
 			}
 		}
 	}
-	putchar('\n');
 	return (0);
 }
-
