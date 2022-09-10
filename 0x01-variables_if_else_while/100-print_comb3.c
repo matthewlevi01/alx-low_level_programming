584499f4213943978de89c861fa758e2f9f02bd9
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-10 04:55:23 +0100

    digits combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio>
+
+/**
+ * main - this is the main function
+ * Description: This function prints all combination of two digits
+ *
+ * Return: returns 0. success value
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		char j;
+
+		for (j = '0'; j <= 9; j++)
+		{
+			if (i != j && i < j)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(',');
+			}
+		}
+	}
+	putchar('\n');
+}
