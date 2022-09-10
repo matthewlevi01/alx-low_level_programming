8be7d55f0b81e2383d3f67c9e051ad1e4f0b1e42
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-10 07:39:53 +0100

    digits combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,13 +15,14 @@ int main(void)
 	{
 		int j;
 
-		for (j = '0'; j <= 9; j++)
+		for (j = i+1; j <= 9; j++)
 		{
-			if (i != j && i < j)
+			putchar(i);
+			putchar(j);
+			if (i !=8 || j !=9)
 			{
-				putchar(i);
-				putchar(j);
 				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
