9a2ebd2275aa5dbbb43a998d42b37e1edb39a69c
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-11 03:06:36 +0100

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Description: prints single digits
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
