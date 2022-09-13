ae8deb58003a110948951028b936161bc275f892
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-13 11:52:41 +0100

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.c"
+
+/**
+ * _abs - entry point
+ * Desctiption: this function returns the absolute value of a parameter
+ * @n: the number to be tested
+ * Return: absolute value
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+	return (0);
+}
