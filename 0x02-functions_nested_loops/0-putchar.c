bf43f449351e6402f5ed7bb9b5becf9f458e1034
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-13 10:56:37 +0100

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main  - this is the executor function
+ * Description: this function prints _putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	char c[] = "_putchar"
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
