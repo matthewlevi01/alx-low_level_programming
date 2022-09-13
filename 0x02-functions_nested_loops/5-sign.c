7c44f895273a9df41f5f485df18ebc9d495be76f
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-13 11:44:19 +0100

    push sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - this is the entry
+ * Description: this function prints sign
+ * @n: character to be checked
+ * Return: 1 or 0;
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+	}
+}
+
+
