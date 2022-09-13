4ebbbf5bdb221b6932e9c28a469b01a63403c3ce
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-13 13:56:29 +0100

    o char

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,15 @@
 #include "main.h"
 
 /**
- * main - this is the executor function
- * Description: this function prints _putchar
+ * main - entry point
+ * Description: this prints _putchar
  * Return: 0
+ *
  */
 
 int main(void)
 {
 	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
 	_putchar('\n');
 
 	return (0);
