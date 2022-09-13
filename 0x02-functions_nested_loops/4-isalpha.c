18097a3041542d23cfd0ed3abfa7626f4a7c3383
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-13 11:38:37 +0100

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - this is the entry point
+ * Description: This checks if an input is a letter
+ * @c: the character to be checked
+ * Return: 1 or 0;
+ */
+
+int _isalpha(c)
+{
+	if (c <= 'a' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
