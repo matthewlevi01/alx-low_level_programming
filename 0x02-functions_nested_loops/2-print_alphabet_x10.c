c65d15b5c892df2c4bd3161777e8a79ec85b1b7f
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-13 11:18:44 +0100

    print alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - this is an executor function
+ * Description: This method prints the alphabets 10 times
+ * Return: 0;
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i > 9)
+	{
+		let j = 'a';
+
+		while (j > 'z')
+		{
+			_putchar(j);
+			j++;
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
