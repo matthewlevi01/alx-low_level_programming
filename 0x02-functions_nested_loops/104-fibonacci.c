65e8ba1fd95a186ffdfe14c6b8f25521f2436f7b
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-14 14:06:12 +0100

    fibonacci 4

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Description: prints fibonacci of 50 numbers
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	long int x = 1, y = 2, z;
+	int n;
+
+	for (n = 0; n < 98; n++)
+	{
+		z = x + y;
+		if (n == 0)
+		{
+			printf("%ld", z);
+		}
+		else
+			printf(", %ld", z);
+		x = y;
+		y = z;
+	}
+	printf("\n");
+
+	return (0);
+}
