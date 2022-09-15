881389200f8449b1ae768b699f60afaed3bfd9cd
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-15 22:08:45 +0100

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - prints line
+ * Descripton: prints lines
+ * @n: number of lijnes to prnt (parameter)
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('-');
+		}
+		_putchar('\n');
+	}
+}
