e08d1a5f05c089f209fab7c35bfb3064409c147f
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-15 23:21:25 +0100

    Fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz-buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - executor function
+ * Description: prnts fizz buzz
+ * Return: 0 on success;
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i < 100)
+			printf(" ");
+	}
+}
