1aabc93b9d8153f5c1682230cc6c670b69decfb0
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-15 20:45:43 +0100

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - prints digits fro, 0 - 9
+ * Description: prints numbers
+ * Return: 0 on success
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
