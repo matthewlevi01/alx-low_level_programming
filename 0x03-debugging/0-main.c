5e48e18849c72293b7bed5e58a848fb3ccc4cff6
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-15 05:20:45 +0100

    main .c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - executor function
+ * Description: this tests the positive_or_negative function
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	positive_or_negative(0);
+	return (0);
+}
