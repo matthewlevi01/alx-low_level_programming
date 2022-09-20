6750f8a2ade0e10d5bcb2ee5cfbee4e403d61df7
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-20 04:15:11 +0100

    swap int

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string passed as a parameter
+ * Return: length of string
+ *
+ */
+
+int _strlen(char *s)
+{
+	int counter = 0;
+	int i;
+}
+
