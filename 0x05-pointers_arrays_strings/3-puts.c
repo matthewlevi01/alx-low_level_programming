050e88ab09463a04f37ab642b14099a5ef342166
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-20 04:48:17 +0100

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str: string passed as a paremeter
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
