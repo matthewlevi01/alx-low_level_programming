7cd40a623373c639e1762ebfbc6f72a259d7e2d4
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-20 20:41:17 +0100

    string reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,11 +11,11 @@ void rev_string(char *str)
 {
 	int l = 0, i = 0;
 	char z;
-	
+
 	while (s[i++])
 		l++;
 
-	for (i = l -1; i >= l /2; i--)
+	for (i = l - 1; i >= l / 2; i--)
 	{
 		z = s[i];
 		s[i] = s[l - i - 1];
