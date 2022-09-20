8bf7a0c6944394f690d99ab272d6f5b0a11f34b3
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-20 04:02:25 +0100

    swap int

---
+++ 0x05-pointers_arrays_strings/1-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point of program
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	a = 98;
+	b = 42;
+
+	printf("a=%d, b=%d\n", a, b);
+	swap_int (&a, &b);
+	printf("a=%d, b=%d\n", a, b);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap_int - swaps two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
