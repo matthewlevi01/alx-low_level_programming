46ae0e8646f5c95b1e85888a3c333ac0853fb869
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-21 01:42:54 +0100

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,12 +11,12 @@ void puts_half(char *str)
 {
 	int i = 0;
 	int length_of_the_string = strlen(str);
-	int midPoint = length_of_the_string / 2;
+	int startPoint = length_of_the_string / 2;
 
 	if (length_of_the_string % 2 != 0)
-		midPoint = (length_of_the_string + 1) / 2;
+		startPoint = (length_of_the_string + 1) / 2;
 
-	for (i = midPoint; midPoint < length_of_the_string; i++)
+	for (i = startPoint; startPoint < length_of_the_string; i++)
 	{
 		putchar(str[i]);
 	}
