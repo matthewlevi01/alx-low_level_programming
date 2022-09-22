ec15d8419789950919891bec690283387057582f
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-22 03:22:19 +0100

    to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * string_toupper - converts lowercases to uppercase
+ * @str: string passed as a parameter
+ *
+ * Return: strings in uppercase
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 'a' || str[i] <= 'z')
+		{
+			str[i] -= 'a' - 'z';
+		}
+		i++;
+	}
+
+	return (str);
+}
