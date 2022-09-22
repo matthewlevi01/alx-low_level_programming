826d5d4988498685eacbe7dcb315fa76f5c147f2
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-22 08:48:57 +0100

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,19 +10,19 @@
 
 char *leet(char *str)
 {
-	int i, j = 0;
+	int i = 0, j;
+
 	char letters[] = "aAeEoOtTlL";
 	char code[] = "4433007711";
 
 	while (str[i])
 	{
-		while (letters[j])
+		for (j = 0; j < 10; j++)
 		{
 			if (str[i] == letters[j])
 			{
 				str[i] = code[j];
 			}
-			j++;
 		}
 		i++;
 	}
