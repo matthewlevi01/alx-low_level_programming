76456e72db9b78cc49711547d1864ec5be01df65
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-23 03:19:43 +0100

    print int

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -14,13 +14,13 @@ void print_number(int n)
 	number = n;
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		number = -n;
 	}
 
 	if (number / 10 != 0)
 	{
-		print_number(number / 10);
+		_print_number(number / 10);
 	}
 
 	putchar((number % 10) + '0');
