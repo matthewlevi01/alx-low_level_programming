fafe105a781ba474047391be29fe00157ed1196f
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-23 02:34:24 +0100

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @str: string to be encoded
+ *
+ * Return: the encoded string;
+ */
+
+char *rot13(char *str)
+{
+	int i = 0;
+	int j;
+	char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rotation[] = "NOPQRSTUVWZXYABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (str[i])
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == letters[j])
+			{
+				str[i] = rotation[j];
+			}
+		}
+		i++;
+	}
+	return (str);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,6 +9,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *str);
 char *leet(char *);
+char *rot(char *str);
 
 
 #endif
