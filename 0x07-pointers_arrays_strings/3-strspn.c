1af92c88f5851e4f99f914b8aea8806d87344710
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-27 01:59:53 +0100

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - rreturns thr number of bytes in a string s
+ * @s: string parameter to check
+ * @accept: string parameter to be compared
+ * Return: unsinged int at success
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	int counter = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				counter++;
+		}
+	}
+	return (counter);
+}
