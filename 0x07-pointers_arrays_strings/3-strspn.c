20af02207e78bf8ae30ca4b33fdb1b5340c74da4
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-27 02:01:54 +0100

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,9 +10,9 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i;
-	int j;
-	int counter = 0;
+	unsigned int i;
+	unsigned int j;
+	unsigned int counter = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
