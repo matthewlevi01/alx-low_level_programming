f9313c00ae43c42bc08e64066259be385de4f35b
matthewlevi01 <matthewlevi1990@gmail.com>
2022-09-29 03:31:42 +0100

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+nes (13 sloc) 253 Bytes
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: string to check
+ * Return: the length of a string.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
