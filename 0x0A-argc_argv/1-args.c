8c16babaa33e47e09c96394eda3f97787716429d
matthewlevi01 <matthewlevi1990@gmail.com>
2022-10-03 12:58:09 +0100

    arguments count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry into the program
+ * @argc: argument count
+ * @argv: arguments array
+ * Return: 0 on success
+ *
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	int count = 0;
+
+	printf("%d\n", argc);
+	
+	return (0);
+}
