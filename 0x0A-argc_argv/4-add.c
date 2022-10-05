9c5f7337396e610c527b423d6c4afa06cf7c9b39
matthewlevi01 <matthewlevi1990@gmail.com>
2022-10-05 06:08:39 +0100

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,20 +9,24 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, j, add = 0;
+	int i, j;
+	int sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
+		/* code */
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (!isdigit(argv[i][j]))
 			{
+				/* code */
 				printf("Error\n");
 				return (1);
 			}
 		}
-		add += atoi(argv[i]);
+		sum += atoi(argv[i]);
 	}
-	printf("%d\n", add);
+	printf("%d\n", sum);
 	return (0);
+	
 }
--- 0x0A-argc_argv/tempCodeRunnerFile.c
+++ 0x0A-argc_argv/tempCodeRunnerFile.c
@@ -1,16 +1,1 @@
-#include <stdio.h>
-
-/**
- * main - entry into the program
- * @argc: argument count
- * @argv: arguments array
- * Return: 0 on success
- *
- */
-
-int main(int argc, char *argv[] __attribute__((unused)))
-{
-	printf("%d\n", argc - 1);
-
-	return (0);
-}
+arg
